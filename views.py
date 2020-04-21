1ae17f27a345f96a310a8c8cefb089542402072f
Alexander Rodion Michaud <alexander@prodperfect.com>
2020-04-21 07:23:32 -0400

    Made domino sprites

--- views.py
+++ views.py
@@ -4,10 +4,63 @@ from events import TickEvent
 DIMENSIONS = (900, 1100)
 SQUARES_PER_ROW = 15
 TRAY_HEIGHT = 200
+TILE_WIDTH = DIMENSIONS[0] // SQUARES_PER_ROW
+TILE_LENGTH = (DIMENSIONS[1] - TRAY_HEIGHT) // SQUARES_PER_ROW
 BLACK = (0,0,0)
 SLATE = (112, 128, 144)
 WHITE = (255, 255, 255)
 SILVER = (192, 192, 192)
+
+class DominoSprite(pygame.sprite.Sprite):
+    def __init__(self, group=None, values = (6,6)):
+        pygame.sprite.Sprite.__init__(self, group)
+        surface = pygame.Surface((TILE_WIDTH * 2, TILE_LENGTH))
+        surface.fill(BLACK)
+
+        self.draw_dots(surface, TILE_WIDTH, 0, values[0])
+        self.draw_dots(surface, TILE_WIDTH, TILE_WIDTH, values[1])
+
+        self.image = surface
+        self.rect = surface.get_rect()
+
+    def draw_dots(self, surface, width, origin, n):
+        radius = width // 10
+        if n==0:
+            return
+        elif n==1:
+            pygame.draw.circle(surface, WHITE, (origin + (width // 2), width // 2), radius)
+            return
+        elif n==2:
+            pygame.draw.circle(surface, WHITE, (origin + (width // 4), 3 * width // 4), radius)
+            pygame.draw.circle(surface, WHITE, (origin + (3 * width // 4), width // 4), radius)
+            return
+        elif n==3:
+            pygame.draw.circle(surface, WHITE, (origin + (width // 4), 3 * width // 4), radius)
+            pygame.draw.circle(surface, WHITE, (origin + (width // 2), width // 2), radius)
+            pygame.draw.circle(surface, WHITE, (origin + (3 * width // 4), width // 4), radius)
+            return
+        elif n==4:
+            pygame.draw.circle(surface, WHITE, (origin + (width // 4), width // 4), radius)
+            pygame.draw.circle(surface, WHITE, (origin + (width // 4), 3 * width // 4), radius)
+            pygame.draw.circle(surface, WHITE, (origin + (3 * width // 4), width // 4), radius)
+            pygame.draw.circle(surface, WHITE, (origin + (3 * width // 4), 3 * width // 4), radius)
+            return
+        elif n==5:
+            pygame.draw.circle(surface, WHITE, (origin + (width // 4), width // 4), radius)
+            pygame.draw.circle(surface, WHITE, (origin + (width // 4), 3 * width // 4), radius)
+            pygame.draw.circle(surface, WHITE, (origin + (3 * width // 4), width // 4), radius)
+            pygame.draw.circle(surface, WHITE, (origin + (3 * width // 4), 3 * width // 4), radius)
+            pygame.draw.circle(surface, WHITE, (origin + (width // 2), width // 2), radius)
+            return
+        elif n==6:
+            pygame.draw.circle(surface, WHITE, (origin + (width // 3), width // 4), radius)
+            pygame.draw.circle(surface, WHITE, (origin + (2 * width // 3), width // 4), radius)
+            pygame.draw.circle(surface, WHITE, (origin + (width // 3), 2 * width // 4), radius)
+            pygame.draw.circle(surface, WHITE, (origin + (2 * width // 3), 2 * width // 4), radius)
+            pygame.draw.circle(surface, WHITE, (origin + (width // 3), 3 * width // 4), radius)
+            pygame.draw.circle(surface, WHITE, (origin + (2 * width // 3), 3 * width // 4), radius)
+            return
+
 
 class GameView:
 
@@ -23,6 +76,7 @@ class GameView:
         pygame.display.set_icon(self.logo)
         pygame.display.set_caption("Dominos")
 
+        self.sprites = pygame.sprite.RenderUpdates()
         self.draw_background()
 
         font = pygame.font.Font(None, 80)
@@ -32,16 +86,23 @@ class GameView:
         pygame.display.flip()
 
 
+
     def draw_background(self):
         self.screen.fill(WHITE)
-        tile_width, tile_length = DIMENSIONS[0] / SQUARES_PER_ROW, (DIMENSIONS[1] - TRAY_HEIGHT) / SQUARES_PER_ROW
         for x in range(0, SQUARES_PER_ROW):
             for y in range(0, SQUARES_PER_ROW):
                 if (x + y) % 2 == 0:
-                    pygame.draw.rect(self.screen, SILVER, [tile_width * x, tile_length * y, tile_width, tile_length]) 
+                    pygame.draw.rect(self.screen, SILVER, [TILE_WIDTH * x, TILE_LENGTH * y, TILE_WIDTH, TILE_LENGTH]) 
         
-        pygame.draw.rect(self.screen, SLATE, [0, DIMENSIONS[1] - TRAY_HEIGHT, DIMENSIONS[0], TRAY_HEIGHT])
+        tray = [0, DIMENSIONS[1] - TRAY_HEIGHT, DIMENSIONS[0], TRAY_HEIGHT] 
+        pygame.draw.rect(self.screen, SLATE, tray)
+        
+        # TESTING SPRITE
+        # newSprite = DominoSprite(self.sprites, (0,6))
+        # self.screen.blit(newSprite.image, (450, 450))
+
         pygame.display.update()
+
 
     def Notify(self, event):
         if isinstance(event, TickEvent):
